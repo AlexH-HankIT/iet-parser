050b2cadcdcea3138e148e39a10abc310658b50a
Alexander Hank <alexander.hank@lra-aic-fdb.de>
2016-08-03 11:22:52 +0200

    Add tests

--- tests/GlobalOptionParserAdd/GlobalOptionParserAddTest.php
+++ tests/GlobalOptionParserAdd/GlobalOptionParserAddTest.php
@@ -56,7 +56,9 @@ class GlobalOptionParserTestAdd extends PHPUnit_Framework_TestCase {
 
             $filesystem->delete('iet.test-running.conf');
 
+            // Only needed on windows when line ending is CRLF instead of LF
             //$this->assertEquals(preg_split('/\r\n|\r|\n/', $expectedContent), preg_split('/\r\n|\r|\n/', $contentAfterWrite));
+            
             $this->assertEquals($contentAfterWrite, $expectedContent);
         }
     }
@@ -102,79 +104,87 @@ class GlobalOptionParserTestAdd extends PHPUnit_Framework_TestCase {
      * Test the addOutgoingUser() helper method
      */
     public function testAddOutgoingUserHelperMethod() {
-        // Create new filesystem adapter
-        $local = new Local(__DIR__ . DIRECTORY_SEPARATOR . 'case1_files', LOCK_EX);
+        $dirs = ['case1_files', 'case2_files'];
 
-        // create new filesystem
-        $filesystem = new Filesystem($local);
+        foreach ($dirs as $dir) {
+            // Create new filesystem adapter
+            $local = new Local(__DIR__ . DIRECTORY_SEPARATOR . $dir, LOCK_EX);
 
-        // for testing purposes: copy the sample file. So we don't change the real data
-        $filesystem->copy('iet.sample.conf', 'iet.test-running.conf');
+            // create new filesystem
+            $filesystem = new Filesystem($local);
 
-        // create normalizer instance
-        $parser = new GlobalOptionParser($filesystem, 'iet.test-running.conf');
+            // for testing purposes: copy the sample file. So we don't change the real data
+            $filesystem->copy('iet.sample.conf', 'iet.test-running.conf');
 
-        // create normalizer instance
-        $normalizer = new Normalizer($parser);
+            // create normalizer instance
+            $parser = new GlobalOptionParser($filesystem, 'iet.test-running.conf');
 
-        // normalize the file
-        $normalizer->write();
+            // create normalizer instance
+            $normalizer = new Normalizer($parser);
 
-        if ($normalizer->check()) {
-            try {
-                $parser->addOutgoingUser("user", "password")->write();
-            } catch (DuplicationErrorException $e) {
-                $this->assertEquals($e->getMessage(), 'The option OutgoingUser user password is already set.');
+            // normalize the file
+            $normalizer->write();
+
+            if ($normalizer->check()) {
+                try {
+                    $parser->addOutgoingUser("user", "password")->write();
+                } catch (DuplicationErrorException $e) {
+                    $this->assertEquals($e->getMessage(), 'The option OutgoingUser user password is already set.');
+                }
+            } else {
+                $this->fail("The normalizer did not properly normalize the file!");
             }
-        } else {
-            $this->fail("The normalizer did not properly normalize the file!");
+
+            $contentAfterWrite = $filesystem->read('iet.test-running.conf');
+            $expectedContent = $filesystem->read('iet.expected.testAddOutgoingUserHelperMethod.conf');
+
+            $filesystem->delete('iet.test-running.conf');
+
+            $this->assertEquals($contentAfterWrite, $expectedContent);
         }
-
-        $contentAfterWrite = $filesystem->read('iet.test-running.conf');
-        $expectedContent = $filesystem->read('iet.expected.testAddOutgoingUserHelperMethod.conf');
-
-        $filesystem->delete('iet.test-running.conf');
-
-        $this->assertEquals(preg_split('/\r\n|\r|\n/', $expectedContent), preg_split('/\r\n|\r|\n/', $contentAfterWrite));
     }
 
     /**
      * Test the addIncomingUser() helper method
      */
     public function testAddIncomingUserHelperMethod() {
-        // Create new filesystem adapter
-        $local = new Local(__DIR__ . DIRECTORY_SEPARATOR . 'case1_files', LOCK_EX);
+        $dirs = ['case1_files', 'case2_files'];
 
-        // create new filesystem
-        $filesystem = new Filesystem($local);
+        foreach ($dirs as $dir) {
+            // Create new filesystem adapter
+            $local = new Local(__DIR__ . DIRECTORY_SEPARATOR . $dir, LOCK_EX);
 
-        // for testing purposes: copy the sample file. So we don't change the real data
-        $filesystem->copy('iet.sample.conf', 'iet.test-running.conf');
+            // create new filesystem
+            $filesystem = new Filesystem($local);
 
-        // create normalizer instance
-        $parser = new GlobalOptionParser($filesystem, 'iet.test-running.conf');
+            // for testing purposes: copy the sample file. So we don't change the real data
+            $filesystem->copy('iet.sample.conf', 'iet.test-running.conf');
 
-        // create normalizer instance
-        $normalizer = new Normalizer($parser);
+            // create normalizer instance
+            $parser = new GlobalOptionParser($filesystem, 'iet.test-running.conf');
 
-        // normalize the file
-        $normalizer->write();
+            // create normalizer instance
+            $normalizer = new Normalizer($parser);
 
-        if ($normalizer->check()) {
-            try {
-                $parser->addIncomingUser("user", "password")->write();
-            } catch (DuplicationErrorException $e) {
-                $this->assertEquals($e->getMessage(), 'The option IncomingUser user password is already set.');
+            // normalize the file
+            $normalizer->write();
+
+            if ($normalizer->check()) {
+                try {
+                    $parser->addIncomingUser("user", "password")->write();
+                } catch (DuplicationErrorException $e) {
+                    $this->assertEquals($e->getMessage(), 'The option IncomingUser user password is already set.');
+                }
+            } else {
+                $this->fail("The normalizer did not properly normalize the file!");
             }
-        } else {
-            $this->fail("The normalizer did not properly normalize the file!");
+
+            $contentAfterWrite = $filesystem->read('iet.test-running.conf');
+            $expectedContent = $filesystem->read('iet.expected.testAddIncomingUserHelperMethod.conf');
+
+            $filesystem->delete('iet.test-running.conf');
+
+            $this->assertEquals($contentAfterWrite, $expectedContent);
         }
-
-        $contentAfterWrite = $filesystem->read('iet.test-running.conf');
-        $expectedContent = $filesystem->read('iet.expected.testAddIncomingUserHelperMethod.conf');
-
-        $filesystem->delete('iet.test-running.conf');
-
-        $this->assertEquals(preg_split('/\r\n|\r|\n/', $expectedContent), preg_split('/\r\n|\r|\n/', $contentAfterWrite));
     }
 }
