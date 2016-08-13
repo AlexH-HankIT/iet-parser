7a982604114500a820da5290233df6f7a5544fa2
MrCrankHank <mail@alexander-hank.de>
2016-08-13 09:02:40 +0200

    Add tests for ProcParser

---
+++ tests/ProcParser/ProcParserTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace MrCrankHank\IetParser\ProcParser;
+
+use MrCrankHank\IetParser\Parser\ProcParser;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use MrCrankHank\IetParser\TestTrait;
+use PHPUnit_Framework_TestCase;
+
+class ProcParserTest extends PHPUnit_Framework_TestCase
+{
+    use TestTrait;
+
+    public static function getSessionSingleProvider()
+    {
+        return [
+            // get data for single iqn identified by iqn (one session)
+            ['session.1', 'iqn.2014-08.local.example.san01:ps', 1,
+                [
+                    'iqn' => 'iqn.2014-08.local.example.san01:ps',
+                    'tid' => '1',
+                    '4786175038521408' => [
+                        'initiator' => 'iqn.1991-05.com.microsoft:ps.example.local',
+                        'cid' => '1',
+                        'ip' => '192.168.100.51',
+                        'state' => 'active',
+                        'hd' => 'none',
+                        'dd' => 'none'
+                    ]
+                ]
+            ],
+
+            // get data for single iqn identified by iqn (two sessions)
+            ['session.1', 'iqn.2014-08.local.example.san01:server21', 12,
+                [
+                    'iqn' => 'iqn.2014-08.local.example.san01:server21',
+                    'tid' => '12',
+                    '2814750689919040' => [
+                        'initiator' => 'iqn.1991-05.com.microsoft:server21.example.local',
+                        'cid' => '1',
+                        'ip' => '192.168.100.11',
+                        'state' => 'active',
+                        'hd' => 'none',
+                        'dd' => 'none'
+                    ],
+                    '2343447334519040' => [
+                        'initiator' => 'iqn.1991-05.com.microsoft:server',
+                        'cid' => '1',
+                        'ip' => '8.8.8.8',
+                        'state' => 'active',
+                        'hd' => 'none',
+                        'dd' => 'none'
+                    ]
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * Retrieve data from a iqn identified by iqn and tid.
+     * Compare them to prove that they return the same content.
+     *
+     * @param $file
+     * @param $target
+     * @param $tid
+     * @param $expectedData
+     *
+     * @dataProvider getSessionSingleProvider
+     */
+    public function testGetSessionSingle($file, $target, $tid, $expectedData)
+    {
+        $dir = __DIR__ . DIRECTORY_SEPARATOR . 'files';
+
+        $local = new Local($dir, LOCK_EX);
+
+        $filesystem = new Filesystem($local);
+
+        $parser = new ProcParser($filesystem, $file);
+
+        $iqnData = $parser->getSession($target);
+
+        $tidData = $parser->getSession($tid);
+
+        $this->assertEquals($iqnData, $expectedData);
+
+        $this->assertEquals($tidData, $expectedData);
+    }
+
+    public function testGetSessionMultiple()
+    {
+
+    }
+
+    public function testGetVolume()
+    {
+
+    }
+}
